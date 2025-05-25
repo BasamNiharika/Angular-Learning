da32b64c333dadaf4485244c34fb554fa3626c9e
Niharika <niharika.basam@solugenix.com>
2025-05-25 22:48:44 +0530

    Life cycle hooks are implemented

--- My-Angular-App/src/app/lifecycle-hooks/lifecycle-hooks.component.ts
+++ My-Angular-App/src/app/lifecycle-hooks/lifecycle-hooks.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { TestComponent } from "../test/test.component";
+import { AfterContentInit, Component, DoCheck } from '@angular/core';
+import { TestComponent } from '../test/test.component';
 
 @Component({
   selector: 'app-lifecycle-hooks',
@@ -7,19 +7,50 @@ import { TestComponent } from "../test/test.component";
   templateUrl: './lifecycle-hooks.component.html',
   styleUrl: './lifecycle-hooks.component.css',
 })
-export class LifecycleHooksComponent {
-  constructor(){
-    console.log('Constructor is called....')  //called first then ngOnInit
+export class LifecycleHooksComponent implements AfterContentInit, DoCheck {
+  constructor() {
+    console.log('Constructor is called....'); //called first then ngOnInit
   }
 
   // ngOnchanges
   value: string = 'value2';
-  updateValue(){
-    this.value='Lifecycle component';
+  updateValue() {
+    this.value = 'Lifecycle component';
   }
 
-  ngOnInit(){
+  ngOnInit() {
     console.log('ngOnInit:Compoenent is called ');
   }
 
+  //ngDoCheck hook
+  user = {
+    name: 'UserA',
+  };
+  changeUserName() {
+    this.user.name = 'UserC';
+  }
+
+  //ngAFterContentInit hook
+  dataFromParent: string = '';
+
+  sendDataToChild(): void {
+    let random = Math.floor(Math.random() * 10);
+    this.dataFromParent = 'Random Number:' + random;
+  }
+
+  //ngAfterContentInit hook
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit hook is invoked...');
+  }
+
+  //ngAfterViewInit hook
+  ngDoCheck(): void {
+    console.log('Do changes called from parent for ngAfterViewInit purpose');
+  }
+
+  //ngOnDestroy hook
+  displayComponent: boolean = true;
+  toggle(): void {
+    this.displayComponent = !this.displayComponent;
+  }
 }
--- My-Angular-App/src/app/test/test.component.ts
+++ My-Angular-App/src/app/test/test.component.ts
@@ -1,12 +1,39 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  ElementRef,
+  ViewChild,
+  ContentChild,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [
+    // JsonPipe
+  ],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css',
 })
-export class TestComponent implements OnChanges {
+export class TestComponent
+  implements
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  // ngOnChanges hook
   @Input() inputValue: string = '';
   previousValue: string | undefined;
   currentValue: string | undefined;
@@ -17,6 +44,59 @@ export class TestComponent implements OnChanges {
       this.previousValue = changes['inputValue'].previousValue;
       this.currentValue = changes['inputValue'].currentValue;
     }
+    if (changes['user']) {
+      console.log('user changed');
+    }
     console.log(changes);
+  }
+
+  //ngDoCheck hook
+  @Input() user: any; //ngOnChanges won't detect changes when user.name changed inside lifecycle component so we use ngDoCheck()
+  private previousUser: string | undefined;
+
+  ngDoCheck(): void {
+    if (this.user && this.user.name != this.previousUser) {
+      this.previousUser = this.user.name;
+      console.log('ngDoCheck called - value changed', this.user.name);
+    }
+    console.log('ngDoCheck called - value changed');
+  }
+  //Note: So here, ngDoCheck manually checks for the changes within the user object's name property.
+  //   -> Never use ngOnChanges and ngDocheck together in the same component, it causes unwanted behaviour.
+
+  //ngAfterContentInit hook
+  @ViewChild('wrapper') wrapper!: ElementRef;
+  @ContentChild('contentWrapper') content!: ElementRef;
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit() hook is invoked...');
+    console.log('ngAfterContentInit() - wrapper', this.wrapper);
+    console.log('ngAfterContentInit() - content', this.content);
+  }
+
+  //ngAfterContentChecked hook
+  // It is mostly used for the tasks that involve checking and responding to the changes on the content projected
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked() hook is called');
+  }
+
+  //ngAfterViewInit hook
+  // It is  called after ngAfterContentChecked() hook is called
+  ngAfterViewInit(): void {
+    const divElement: HTMLElement = this.wrapper.nativeElement;
+    divElement.style.color = 'maroon';
+    divElement.style.fontSize = '15px';
+    divElement.style.fontWeight = '300';
+    console.log('ngAfterViewInit() hook is called');
+  }
+
+  //ngAfterViewChecked hook
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked() hook is called... ');
+  }
+
+  //ngOnDestroy hook
+  ngOnDestroy(): void {
+    console.log('ngDestroy() hook is called...');
   }
 }
