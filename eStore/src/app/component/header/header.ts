6c43acc18f8156bd962893b8003ea99a5efae510
Niharika <niharika.basam@solugenix.com>
2025-08-02 17:20:08 +0530

    Converted eStore from submodule to regular folder

---
+++ eStore/src/app/component/header/header.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {
+  faUserCircle,
+  faHeart,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  imports: [FontAwesomeModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  faSearch = faSearch;
+  faUserCircle = faUserCircle;
+  faHeart = faHeart;
+  faShoppingCart = faShoppingCart;
+
+  constructor() {
+    console.log('testing');
+  }
+}
