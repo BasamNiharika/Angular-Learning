67e6bb4a6e6901a8c5264e278ca9bbc147f72399
Niharika <niharika.basam@solugenix.com>
2025-08-07 12:47:14 +0530

    Connection from node.js to MySQL server completed, also done basics of extracting data from database

---
+++ eStore-backend/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+// mySQL driver
+const mysql = require("mysql2")
+const app = express();
+const PORT = 5001;
+
+// pool connection is a best practice for handling multiple database queries efficiently.
+const pool = mysql.createPool({
+  host:"localhost",
+  user: "root",
+  password:"Niha@1109",
+  database:"estore",
+  port: 3306,
+  multipleStatements: true
+});
+
+// app.get("/", (req, res) => {
+//   // to estable a connection with database
+//   pool.getConnection((err,connection)=>{
+//     if(err){
+//       res.status(500).send(err);
+//     }
+//     else{
+//       pool.query("select * from categories",(error,categories)=>{
+//         if(error) res.status(500).send(error);
+//         else res.status(200).send(categories);
+//       })
+//       // res.status(200).send("Connection Established.")
+//     }
+//   })
+// });
+
+// without getConnection() method.
+app.get("/", (req, res) => {
+  pool.getConnection((err,connection)=>{
+      pool.query("select * from categories",(error,categories)=>{
+        if(error) res.status(500).send(error);
+        else res.status(200).send(categories);
+      })
+  })
+});
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello000</h1>");
+// });
+
+const server = app.listen(PORT, () => {
+  console.log("App running on port - 5001");
+});
