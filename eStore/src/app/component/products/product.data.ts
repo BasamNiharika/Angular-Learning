85ab098e06ac9d23a1195d0227e36c0e5492589f
Niharika <niharika.basam@solugenix.com>
2025-08-07 09:52:26 +0530

    rating component done

--- eStore/src/app/component/products/product.data.ts
+++ eStore/src/app/component/products/product.data.ts
@@ -6,53 +6,62 @@ export const products: ProductListItem[] = [
     product_name: 'Jacket',
     product_img: 'shop-1',
     price: 100,
+    rating:3.4
   },
   {
     id: 2,
     product_name: 'Purse',
     product_img: 'shop-2',
     price: 25,
+    rating:4
   },
   {
     id: 3,
     product_name: 'Dress',
     product_img: 'shop-3',
     price: 45,
+    rating:5
   },
   {
     id: 4,
     product_name: 'Denim Jeans',
     product_img: 'shop-4',
     price: 50,
+    rating:2.5
   },
   {
     id: 5,
     product_name: 'Laced Boots',
     product_img: 'shop-5',
     price: 55,
+    rating:3.5
   },
   {
     id: 6,
     product_name: 'Back pack',
     product_img: 'shop-6',
     price: 70,
+    rating:4
   },
   {
     id: 7,
     product_name: 'earings',
     product_img: 'shop-7',
     price: 75,
+    rating:5
   },
   {
     id: 8,
     product_name: 'scarf',
     product_img: 'shop-8',
     price: 50,
+    rating:2.8
   },
   {
     id: 9,
     product_name: 'Boots',
     product_img: 'shop-9',
     price: 120,
+    rating:3.9
   },
 ];
--- eStore/src/app/component/products/products.ts
+++ eStore/src/app/component/products/products.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ProductService } from './product-service';
 import { ProductListItem } from './product.type';
 import { CommonModule } from '@angular/common';
+import { Ratings } from '../ratings/ratings';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule],
+  imports: [CommonModule,Ratings],
   templateUrl: './products.html',
   styleUrl: './products.css',
   providers: [ProductService],
